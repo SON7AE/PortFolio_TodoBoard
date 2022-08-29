d80332496e1da66f5d8eaf70a8c2e1335921f3fe
SON7AE <bobsot0225@gmail.com>
2022-08-29 21:34:05 +0900

    3. 컴포넌트 Atomic Design Pattern 작업 - Button Component

---
+++ src/components/atoms/CreateBoardButton.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="button">
+        <slot name="label"></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    setup() {},
+};
+</script>
+
+<style lang="scss" scoped>
+@import '~/scss/main.scss';
+
+.button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    width: 234px;
+    height: 37px;
+
+    background-color: $board--color--normal;
+    border: 1px solid $border--color;
+    border-radius: 4px;
+    color: $font--color--normal;
+    font-family: 'Roboto', sans-serif;
+    font-size: 14px;
+    cursor: pointer;
+
+    &:hover {
+        background-color: $board--color--hover;
+    }
+    &__active {
+        background-color: $board--color--active;
+    }
+}
+</style>
