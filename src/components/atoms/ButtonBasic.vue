cae8e40102cd2919ba3706733be3d6975f32375e
박성재 <bobsot0225@gmail.com>
2022-09-02 09:38:27 +0900

    3. 컴포넌트 Atomic Design Pattern 작업

---
+++ src/components/atoms/ButtonBasic.vue
@@ -0,0 +1,18 @@
+<template>
+  <button class="button-basic">
+    {{ data }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style></style>
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,13 @@
 <template>
   <h1>Home.vue 컴포넌트 입니다.</h1>
+  <ButtonBasic :data="'Add New Page'" />
 </template>
 
 <script>
-export default {};
+import ButtonBasic from '../components/atoms/ButtonBasic.vue';
+export default {
+  components: { ButtonBasic },
+};
 </script>
 
 <style></style>
